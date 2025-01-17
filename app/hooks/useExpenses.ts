4e118a030dce5082abd31a063b5a3018e3e98220
Sciabarrasi <gianluca_sciabarrasi@hotmail.com>
2025-01-17 20:20:07 -0300

    Agregado de Dashboard, CRUD completo y funcionando

---
+++ app/hooks/useExpenses.ts
@@ -0,0 +1,131 @@
+import { useState, useCallback } from "react";
+import axios from "axios";
+import { Expense } from "@prisma/client";
+
+export const useExpenses = () => {
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchExpenses = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.get("/api/expense");
+      setExpenses(response.data);
+    } catch (err: any) {
+      setError(err?.message || "Error al hacer fetch a expenses");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const addExpense = useCallback(async (newExpense: Partial<Expense>) => {
+    setLoading(true);
+    setError(null);
+
+    if (!newExpense.title || !newExpense.amount) {
+      setError("El título y monto son requeridos.");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const expenseData = {
+        name: newExpense.title,
+        amount: newExpense.amount,
+      };
+
+      const response = await axios.post("/api/expense", expenseData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      setExpenses((prev) => [...prev, response.data]);
+    } catch (err: any) {
+      setError(err?.response?.data?.error || err?.message || "Error al agregar gasto.");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const updateExpense = useCallback(
+    async (id: string, updateExpense: Partial<Expense>) => {
+      setLoading(true);
+      setError(null);
+  
+      if (!id || !updateExpense.title || !updateExpense.amount || isNaN(Number(updateExpense.amount))) {
+        setError("El ID, título y monto son requeridos para actualizar un gasto y el monto debe ser válido.");
+        setLoading(false);
+        return;
+      }
+  
+      const sanitizedUpdateExpense = {
+        id,
+        title: updateExpense.title.trim(),
+        amount: Number(updateExpense.amount),
+        description: updateExpense.description?.trim() || null,
+      };
+  
+      try {
+        const response = await axios.put(`/api/expense`, sanitizedUpdateExpense);
+  
+        if (response.data) {
+          setExpenses((prev) =>
+            prev.map((expense) =>
+              Number(expense.id) === Number(id)
+                ? {
+                    ...expense,
+                    title: response.data.title,
+                    amount: response.data.amount,
+                    description: response.data.description,
+                  }
+                : expense
+            )
+          );
+        }
+      } catch (err: any) {
+        setError(err?.response?.data?.error || err?.message || "Error al actualizar el gasto.");
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setExpenses, setError, setLoading]
+  );
+  
+
+  const deleteExpense = async (id: string) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(`/api/expense?id=${id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        setExpenses((prev) => prev.filter((expense) => expense.id !== Number(id)));
+      } else {
+        const { error } = await response.json();
+        setError(error || "Error al eliminar el gasto.");
+      }
+    } catch (err) {
+      console.error(err);
+      setError("Error al eliminar el gasto.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    expenses,
+    loading,
+    error,
+    fetchExpenses,
+    addExpense,
+    updateExpense,
+    deleteExpense,
+  };
+};
